94ed7cc37792b5724c0ad7ef6d0b1736980f3c76
victor vancan <victorvancan1@gmail.com>
2023-09-23 12:29:07 -0300

    update servicos

--- js/validarCadastro.js
+++ js/validarCadastro.js
@@ -102,6 +102,8 @@ class Validator {
       if(!re.test(email)) {
         this.printMessage(input, errorMessage);
       }
+
+      let emailcorreto = email;
   
     }
   
@@ -152,7 +154,8 @@ class Validator {
   
         this.printMessage(input, errorMessage);
       }
-  
+      
+      let senha = input;
     }
   
     // método para imprimir mensagens de erro
@@ -193,4 +196,6 @@ class Validator {
     e.preventDefault();
   
     validator.validate(form);
+
   });
+  
