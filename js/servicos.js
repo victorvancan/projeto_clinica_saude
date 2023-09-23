de96c4ec05b219dd8afe1f7d80e501c711341368
victor vancan <victorvancan1@gmail.com>
2023-09-23 11:56:28 -0300

    carrinho att

--- js/servicos.js
+++ js/servicos.js
@@ -1,55 +1,120 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const addButtonList = document.querySelectorAll(".add-to-cart");
-    const removeButtonList = document.querySelectorAll(".remove-from-cart");
-    const quantityInputList = document.querySelectorAll("input[type='number']");
-    const carrinhoList = document.getElementById("itens-carrinho");
-    const totalCarrinho = document.getElementById("total-carrinho");
-    const carrinho = []; // Array para armazenar os itens no carrinho
-    
-    addButtonList.forEach(function (button, index) {
-     button.addEventListener("click", function () {
-         // Recuperar o preço do atributo data-preco do botão clicado
-         const itemPrice = parseFloat(button.getAttribute("data-preco"));
-         const itemQuantity = parseInt(quantityInputList[index].value);
-    
-         // Adicionar o item ao carrinho com seu próprio preço
-         carrinho.push({ preco: itemPrice, quantidade: itemQuantity });
-         atualizarCarrinho();
-     });
-    });
-    
-    removeButtonList.forEach(function (button, index) {
-     button.addEventListener("click", function () {
-         // Adicionar lógica para remover o item do carrinho
-         carrinho.splice(index, 1);
-         atualizarCarrinho();
-     });
-    });
-    
-    quantityInputList.forEach(function (input, index) {
-     input.addEventListener("change", function () {
-         // Adicionar lógica para alterar a quantidade do item no carrinho
-         carrinho[index].quantidade = parseInt(input.value);
-         atualizarCarrinho();
-     });
-    });
-    
-    function atualizarCarrinho() {
-     // Limpar a lista do carrinho
-     carrinhoList.innerHTML = "";
-    
-     // Calcular o total do carrinho
-     let total = 0;
-    
-     carrinho.forEach(function (item) {
-         const listItem = document.createElement("li");
-         const subtotal = item.preco * item.quantidade;
-         listItem.textContent = `Preço: R$ ${item.preco.toFixed(2)} - Quantidade: ${item.quantidade} - Subtotal: R$ ${subtotal.toFixed(2)}`;
-         carrinhoList.appendChild(listItem);
-    
-         total += subtotal;
-     });
-    
-     totalCarrinho.textContent = `Total: R$ ${total.toFixed(2)}`;
+if (document.readyState == 'loading') {
+    document.addEventListener('DOMContentLoaded', ready)
+  } else {
+    ready()
+  }
+  
+  var totalAmount = "0,00"
+  
+  function ready() {
+    // Botão remover produto
+    const removeCartProductButtons = document.getElementsByClassName("remove-product-button")
+    for (var i = 0; i < removeCartProductButtons.length; i++) {
+      removeCartProductButtons[i].addEventListener("click", removeProduct)
     }
-    });
+  
+    // Mudança valor dos inputs
+    const quantityInputs = document.getElementsByClassName("product-qtd-input")
+    for (var i = 0; i < quantityInputs.length; i++) {
+      quantityInputs[i].addEventListener("change", checkIfInputIsNull)
+    }
+  
+    // Botão add produto ao carrinho
+    const addToCartButtons = document.getElementsByClassName("button-hover-background")
+    for (var i = 0; i < addToCartButtons.length; i++) {
+      addToCartButtons[i].addEventListener("click", addProductToCart)
+    }
+  
+    // Botão comprar
+    const purchaseButton = document.getElementsByClassName("purchase-button")[0]
+    purchaseButton.addEventListener("click", makePurchase)
+  }
+  
+  function removeProduct(event) {
+    event.target.parentElement.parentElement.remove()
+    updateTotal()
+  }
+  
+  function checkIfInputIsNull(event) {
+    if (event.target.value === "0") {
+      event.target.parentElement.parentElement.remove()
+    }
+  
+    updateTotal()
+  }
+  
+  function addProductToCart(event) {
+    const button = event.target
+    const productInfos = button.parentElement.parentElement
+    const productImage = productInfos.getElementsByClassName("product-image")[0].src
+    const productName = productInfos.getElementsByClassName("product-title")[0].innerText
+    const productPrice = productInfos.getElementsByClassName("product-price")[0].innerText
+  
+    const productsCartNames = document.getElementsByClassName("cart-product-title")
+    for (var i = 0; i < productsCartNames.length; i++) {
+      if (productsCartNames[i].innerText === productName) {
+        productsCartNames[i].parentElement.parentElement.getElementsByClassName("product-qtd-input")[0].value++
+        updateTotal()
+        return
+      }
+    }
+  
+    let newCartProduct = document.createElement("tr")
+    newCartProduct.classList.add("cart-product")
+  
+    newCartProduct.innerHTML =
+      `
+        <td class="product-identification">
+          <img src="${productImage}" alt="${productName}" class="cart-product-image">
+          <strong class="cart-product-title">${productName}</strong>
+        </td>
+        <td>
+          <span class="cart-product-price">${productPrice}</span>
+        </td>
+        <td>
+          <input type="number" value="1" min="0" class="product-qtd-input">
+          <button type="button" class="remove-product-button">Remover</button>
+        </td>
+      `
+    
+    const tableBody = document.querySelector(".cart-table tbody")
+    tableBody.append(newCartProduct)
+    updateTotal()
+  
+    newCartProduct.getElementsByClassName("remove-product-button")[0].addEventListener("click", removeProduct)
+    newCartProduct.getElementsByClassName("product-qtd-input")[0].addEventListener("change", checkIfInputIsNull)
+  }
+  
+  function makePurchase() {
+    if (totalAmount === "0,00") {
+      alert("Seu carrinho está vazio!")
+    } else {   
+      alert(
+        `
+          Obrigado pela sua compra!
+          Valor do pedido: R$${totalAmount}\n
+          Volte sempre :)
+        `
+      )
+  
+      document.querySelector(".cart-table tbody").innerHTML = ""
+      updateTotal()
+    }
+  }
+  
+  // Atualizar o valor total do carrinho
+  function updateTotal() {
+    const cartProducts = document.getElementsByClassName("cart-product")
+    totalAmount = 0
+  
+    for (var i = 0; i < cartProducts.length; i++) {
+      const productPrice = cartProducts[i].getElementsByClassName("cart-product-price")[0].innerText.replace("R$", "").replace(",", ".")
+      const productQuantity = cartProducts[i].getElementsByClassName("product-qtd-input")[0].value
+  
+      totalAmount += productPrice * productQuantity
+    }
+    
+    totalAmount = totalAmount.toFixed(2)
+    totalAmount = totalAmount.replace(".", ",")
+    document.querySelector(".cart-total-container span").innerText = "R$" + totalAmount
+  }
